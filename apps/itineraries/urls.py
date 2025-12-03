af9e76685628f0c8272b7379800f2432f98a0a44
D4RKM4773R <jayalaa1900@alumno.ipn.mx>
2025-12-02 22:44:46 -0600

    URLs de edición, borrado y listado de itinerarios

--- apps/itineraries/urls.py
+++ apps/itineraries/urls.py
@@ -7,14 +7,16 @@ from . import views
 app_name = 'itineraries'
 
 urlpatterns = [
-    path('itineraries/create/', views.create_itinerary_view, name='create_itinerary'),
+    path('itineraries/', views.my_itineraries_view, name='my_itineraries'),
+    path('itineraries/create/', views.create_edit_itinerary_view, name='create_itinerary'),
+    path('itineraries/delete/<int:itinerary_id>/', views.delete_itinerary_view, name='delete_itinerary'),
     path('itineraries/<int:itinerary_id>/add-stops/', views.add_stops_view, name='add_stops'),
     path('itineraries/<int:itinerary_id>/preview/', views.itinerary_preview_view, name='preview_itinerary'),
-    path('itineraries/<int:itinerary_id>/edit/', views.edit_itinerary_view, name='edit_itinerary'),
+    path('itineraries/<int:itinerary_id>/edit/', views.create_edit_itinerary_view, name='edit_itinerary'),
     path('itineraries/<int:itinerary_id>/view/', views.view_itinerary_view, name='view_itinerary'),
 
     path('rate/<int:itinerary_id>/', views.rate_itinerary, name='rate_itinerary'),
-    path('home/', views.home_view, name='home'),
+    path('', views.home_view, name='home'),
 
     # --- NUEVAS RUTAS PARA ACCIONES SOCIALES ---
     path('like/<int:itinerary_id>/', views.toggle_itinerary_like, name='toggle_like'),
